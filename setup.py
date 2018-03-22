d6d33935efaf7207be415a1fcf88e28bf73721fb
Antonio Todorov <taeskow@gmail.com>
2018-03-22 03:35:35 +0100

    Make setup.py publishable

--- setup.py
+++ setup.py
@@ -1,13 +1,110 @@
-from setuptools import setup, find_packages
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'xparser'
+PACKAGE_NAME = 'xrp'
+DESCRIPTION = 'An XResources parser for python applications.'
+KEYWORDS = 'parser XResources'
+URL = 'https://github.com/taesko/xparser'
+EMAIL = 'taeskow@gmail.com'
+AUTHOR = 'Antonio Todorov'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = None
+
+REQUIRED_FOR_INSTALL = []
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.rst' is present in MANIFEST.in file!
+with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    with open(os.path.join(here, PACKAGE_NAME, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
 
 
 setup(
-    name='xparser',
-    author='Antonio Todorov',
-    email='taeskow@gmail.com',
-    license='MIT',
-    version='0.0.1',
-    packages=find_packages(exclude='tests'),
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    keywords=KEYWORDS,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    python_requires=REQUIRES_PYTHON,
+    packages=['xrp'],
+    install_requires=REQUIRED_FOR_INSTALL,
+    tests_require=['pytest', 'pytest-runner'],
     include_package_data=True,
-    tests_require=['pytest', 'pytest-runner']
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Framework :: Pytest'
+
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
