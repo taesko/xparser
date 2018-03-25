5a5123d9aacdd81c71243ec999cc8a014b415b55
Antonio Todorov <taeskow@gmail.com>
2018-03-25 22:48:34 +0300

    Refactor views to inherit from ABCs and clean API. BaseView is now an ABC inherited by BaseDictView (which also inherits abc.Mapping) and BaseListView (which also inherits abc.Sequence).
    View API now seperates XStatements outputted from XParser and parts
    from the statement in which the user is interested (ex. names and values
    of Resources): the x_statements, x_statement and statement_at_line
    properties and methods provide the former and everything else the
    latter.

--- xrp/views.py
+++ xrp/views.py
@@ -1,98 +1,136 @@
-import collections
-import functools
+import collections.abc
+import abc
 
 import itertools
 
 from xrp.match import match_resource
 
 
-def get_base_view(collection):
-    class BaseView(collection):
-        is_initialized = False
-
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            self.is_initialized = True
-
-        @property
-        def x_statements(self):
-            return self.data
-
-        @classmethod
-        def from_parser(cls, parser):
-            raise NotImplementedError()
-
-        def text_at_line(self, line_num):
-            for x_state in self.iterate_statements():
-                if line_num == x_state.line:
-                    return str(x_state)
-            else:
-                raise IndexError("line {} is not in this view".format(line_num))
-
-        def iterate_statements(self):
-            raise NotImplementedError()
-
-        def __getitem__(self, item):
-            return super().__getitem__(item).value
-
-        def __setitem__(self, key, value):
-            if not self.is_initialized:
-                super().__setitem__(key, value)
-            else:
-                raise TypeError("{} object does not support item assignment".format(self.__class__.__name__))
-
-        def __delitem__(self, key):
-            if not self.is_initialized:
-                super().__delitem__(key=key)
-            else:
-                raise TypeError("{} object does not support item deletion".format(self.__class__.__name__))
-
-    return BaseView
-
-
-class BaseDictView(get_base_view(collections.UserDict)):
+class BaseView(abc.ABC):
 
     @classmethod
+    @abc.abstractmethod
     def from_parser(cls, parser):
         raise NotImplementedError()
 
-    def iterate_statements(self):
-        return self.x_statements.values()
-
-
-class BaseListView(get_base_view(collections.UserList)):
-    @classmethod
-    def from_parser(cls, parser):
+    @property
+    @abc.abstractmethod
+    def x_statements(self):
         raise NotImplementedError()
 
-    def iterate_statements(self):
-        return self.x_statements
+    @abc.abstractmethod
+    def x_statement(self, *args, **kwargs):
+        raise NotImplementedError()
+
+    def statement_at_line(self, line_num):
+        for x_state in self.x_statements:
+            if line_num == x_state.line:
+                return x_state
+        else:
+            raise IndexError("line {} is not in this view".format(line_num))
+
+    def text_at_line(self, line_num):
+        return str(self.statement_at_line(line_num))
+
+
+class BaseDictView(BaseView, collections.abc.Mapping):
+    @property
+    @abc.abstractmethod
+    def dict_data(self):
+        raise NotImplementedError()
+
+    @property
+    def x_statements(self):
+        return self.dict_data.values()
+
+    def x_statement(self, key):
+        return self.dict_data[key]
+
+    def __len__(self):
+        return len(self.dict_data)
+
+    def __iter__(self):
+        for key in self.dict_data:
+            yield key
+
+
+class BaseListView(BaseView, collections.abc.Mapping):
+    @property
+    @abc.abstractmethod
+    def list_data(self):
+        raise NotImplementedError()
+
+    @property
+    def x_statements(self):
+        return self.list_data
+
+    def x_statement(self, index):
+        return self.list_data[index]
+
+    def __len__(self):
+        return len(self.list_data)
+
+    def __iter__(self):
+        for index in range(len(self)):
+            yield self[index]
 
 
 class Resources(BaseDictView):
+
+    def __init__(self, resources):
+        self.resource_statements = resources
+
+    @property
+    def dict_data(self):
+        return self.resource_statements
 
     @classmethod
     def from_parser(cls, parser):
         return cls(parser.resources)
 
+    def __getitem__(self, key):
+        return self.resource_statements[key].value
+
     def filter(self, string):
         resources = {}
-        for res_id, value in self.x_statements.items():
+        for res_id in self:
             if match_resource(resource=res_id, string=string):
-                resources[res_id] = value
+                resources[res_id] = self.x_statement(res_id)
         return self.__class__(resources)
 
 
 class Definitions(BaseDictView):
+
+    def __init__(self, definitions):
+        self.define_statements = definitions
+
     @classmethod
     def from_parser(cls, parser):
         return cls(parser.defines)
 
+    @property
+    def dict_data(self):
+        return self.define_statements
+
+    def __getitem__(self, key):
+        return self.define_statements[key].value
+
 
 class Comments(BaseListView):
+
+    def __init__(self, comment_statements):
+        self.comment_statements = comment_statements
+
     @classmethod
     def from_parser(cls, parser):
         return cls(parser.comments)
+
+    @property
+    def list_data(self):
+        return self.comment_statements
+
+    def __getitem__(self, key):
+        return self.comment_statements[key].value
 
 
 class EmptyLines(collections.UserList):
