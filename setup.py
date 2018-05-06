d08f923f4bffe05a5f8bfea2a0009dbdf8cbe584
Antonio Todorov <taeskow@gmail.com>
2018-05-06 09:08:19 +0300

    Add a test_upload command to setup.py.
    test_upload uploads packages to test.pypi.org/legacy and doesn't push git tags.

--- setup.py
+++ setup.py
@@ -81,6 +81,39 @@ class UploadCommand(Command):
         sys.exit()
 
 
+class TestUploadCommand(Command):
+    """Support setup.py test_upload."""
+
+    description = 'Build and publish the package to TestPyPI.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel distribution…')
+        os.system('{0} setup.py sdist bdist_wheel'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload --repository-url https://test.pypi.org/legacy/ dist/*')
+
+        sys.exit()
+
+
 setup(
     name=NAME,
     version=about['__version__'],
@@ -114,5 +147,6 @@ setup(
     # $ setup.py publish support.
     cmdclass={
         'upload': UploadCommand,
+        'test_upload': TestUploadCommand
     },
 )
